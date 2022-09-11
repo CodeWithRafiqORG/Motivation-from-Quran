4eeef8842d273cdd6b0839d207868ad3b775878b
rafiqul-altersense <rafiqul.altersense@gmail.com>
2022-09-11 18:49:20 +0600

    Added Dynamic image Funcatations

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@
 // https://mail.google.com/
 // https://github.com/
 
-let imageUrl = `public/images/${Math.floor(Math.random() * 14)}.jpg`;
+// let imageUrl = `public/images/${Math.floor(Math.random() * 14)}.jpg`;
 
 let startTime = () => {
   const today = new Date();
@@ -36,10 +36,55 @@ let addTitle = () => {
   setTimeout(addTitle, 20000);
 };
 
+// let loadImage = async () => {
+//   let req = await fetch(
+//     "https://api.unsplash.com/photos/random?page=1&query=natural&client_id=ej5PjFlElrFnuzhpXL8-UYlOEmGzo6chhU9ojnUk9cg&h=800&w=1200"
+//   );
+//   let data = await req.json();
+//   return await data.urls.full;
+// };
+
+const getBase64FromUrl = async () => {
+  let base64image = await chrome.storage.local.get("image");
+  console.log("====================================");
+  console.log("base64image: " + base64image.data);
+  console.log("====================================");
+  if (base64image.data === undefined) {
+    console.log("If-----------");
+    let url =
+      "https://api.unsplash.com/photos/random?page=1&query=natural&client_id=ej5PjFlElrFnuzhpXL8-UYlOEmGzo6chhU9ojnUk9cg&h=800&w=1200";
+    const req = await fetch(url);
+    let data1 = await req.json();
+    let img_url = await data1.urls.full;
+
+    const data = await fetch(img_url);
+    const blob = await data.blob();
+    return new Promise((resolve) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(blob);
+      reader.onload = () => {
+        chrome.storage.local.set({ image: reader.result }, function (data) {
+          console.log("data---->", data);
+        });
+        resolve(reader.result);
+      };
+
+      reader.onerror = (error) => reject(error);
+    });
+  } else {
+    console.log("Else---");
+    return base64image.data;
+  }
+};
+
 document.addEventListener("DOMContentLoaded", async function () {
+  let base64image = await getBase64FromUrl();
+
+  console.log("Base64 image loaded: " + base64image);
+
   document.getElementById("container").style.backgroundImage =
-    "url(" + imageUrl + ")";
-  console.log(imageUrl);
+    "url(" + base64image + ")";
+
   addTitle();
   startTime();
 
